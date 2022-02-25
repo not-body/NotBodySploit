bbfe80afe49aa50479e07ff5c49af18027d499f9
not-body <81175625+not-body@users.noreply.github.com>
2022-02-25 20:59:26 +0300

    kurulum termux

--- build-termux.sh
+++ build-termux.sh
@@ -1,6 +1,6 @@
 #! /bin/bash 
 apt-get -y install php openssh git wget
-a= read -p "Metasploit-Framework Kurulusun Mu [Y/N]: "
+a= read -p "Metasploit-Framework Kurulsun Mu [Y/N]: "
 if [[ a == "y" || a == "Y" || a == "Yes" || a == "yes" || "YES" ]]; then
 cd $HOME
 pkg update && pkg upgrade -y &&pkg install postgresql && pkg install openssh wget curl git -y && wget https://raw.githubusercontent.com/gushmazuko/metasploit_in_termux/master/metasploit.sh && chmod +x  metasploit.sh && ./metasploit.sh
@@ -8,6 +8,6 @@ fi
 cd /data/data/com.termux/files/usr/share
 git clone https://github.com/not-body/NotBodySploit
 cd /data/data/com.termux/files/usr/bin
-echo python3 /usr/share/NotBodySploit/notbody-termux.py > notbody
+echo python3 /data/data/com.termux/files/usr/share/NotBodySploit/notbody-termux.py > notbody
 chmod +x notbody
 echo Kurulum Tamamlandı
